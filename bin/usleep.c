83740d0ff372d9e754851df327d94d67537a4003
Michael Warren <72041795+PsychoCod3r@users.noreply.github.com>
2024-12-06 09:53:58 -0500

    Changed usleep.c so error messages are printed to stderr instead of stdout.

--- bin/usleep.c
+++ bin/usleep.c
@@ -8,18 +8,18 @@
 int main( int argc, char **argv ){
 	// Make sure there is only one argument:
 	if( argc == 1 ){
-		printf( "%s: Needs an integer value (microseconds to sleep).\n", argv[0] );
+		fprintf( stderr, "%s: Needs an integer value (microseconds to sleep).\n", argv[0] );
 		exit( 1 );
 	}
 	if( argc > 2 ){
-		printf( "%s: Too many arguments.\n", argv[0] );
+		fprintf( stderr, "%s: Too many arguments.\n", argv[0] );
 		exit( 1 );
 	}
 	// Make sure the argument is a positive integer:
 	int len = strlen( argv[1] );
 	for( int i = 0; i < len; i++ ){
 		if( argv[1][i] < '0' || argv[1][i] > '9' ){
-			printf( "%s: %s: Argument must be a positive integer.\n", argv[0], argv[1] );
+			fprintf( stderr, "%s: %s: Argument must be a positive integer.\n", argv[0], argv[1] );
 			exit( 2 );
 		}
 	}
